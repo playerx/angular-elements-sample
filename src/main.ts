930d6e6929aaaa2c69afd3f366b9d407c30d2568
Ezeki ZIbzibadze <ez@Green.local>
2017-12-08 07:08:23 +0400

    reduced generated js files number

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import './polyfills';
+
 import { enableProdMode, destroyPlatform, Type, NgModuleFactory, CompilerFactory } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { registerAsCustomElements } from '@angular/elements';
