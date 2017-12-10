799c4e27f4d26e51de8c12890402c99a5addff49
Ezeki ZIbzibadze <ez@Green.local>
2017-12-10 12:35:52 +0400

    updated ui colors for making easier to see difference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,6 +6,16 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular Elements Demo';
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    // first navigation not fired when loading in web component
+    this.router.navigate(['/page1']);
+  }
+
 }
